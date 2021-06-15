c18f034403d684acb969bcddc045436a38fe3868
Siddharth <siddharthauchiha50@gmail.com>
2021-06-15 13:31:44 +0530

    created a modal open close functionality

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+const openModalHandler = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalHandler = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  btnsOpenModel[i].addEventListener('click', openModalHandler);
+}
+
+btnCloseModal.addEventListener('click', closeModalHandler);
+overlay.addEventListener('click', closeModalHandler);
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModalHandler();
+  }
+});
